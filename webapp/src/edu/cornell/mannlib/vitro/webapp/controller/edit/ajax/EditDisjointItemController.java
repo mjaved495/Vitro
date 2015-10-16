bc985c058bf1b4cfa36179e9eca62624346834d5
Amit Mizrahi <am2269@cornell.edu>
2015-10-16 17:56:01 -0400

    add empty classes for AJAX endpoints

---
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/controller/edit/ajax/EditDisjointItemController.java
@@ -0,0 +1,15 @@
+package edu.cornell.mannlib.vitro.webapp.controller.edit.ajax;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EditDisjointItemController extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) {
+		
+	}
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse res) {
+		
+	}
+}
---
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/controller/edit/ajax/EditVClassLabelController.java
@@ -0,0 +1,16 @@
+package edu.cornell.mannlib.vitro.webapp.controller.edit.ajax;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class EditVClassLabelController extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) {
+		
+	}
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse res) {
+		
+	}
+}
