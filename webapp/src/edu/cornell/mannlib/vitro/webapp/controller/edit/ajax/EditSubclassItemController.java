30b1ba9d489e55dbb2a7862bf685d8329330f49b
Amit Mizrahi <am2269@cornell.edu>
2015-10-22 13:54:36 -0400

    write backend code for editing subclass item

--- webapp/src/edu/cornell/mannlib/vitro/webapp/controller/edit/ajax/EditSubclassItemController.java
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/controller/edit/ajax/EditSubclassItemController.java
@@ -1,15 +1,30 @@
 package edu.cornell.mannlib.vitro.webapp.controller.edit.ajax;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import edu.cornell.mannlib.vitro.webapp.beans.VClass;
+import edu.cornell.mannlib.vitro.webapp.dao.VClassDao;
+import edu.cornell.mannlib.vitro.webapp.dao.WebappDaoFactory;
+import edu.cornell.mannlib.vitro.webapp.dao.jena.VClassDaoJena;
+import edu.cornell.mannlib.vitro.webapp.modelaccess.ModelAccess;
 
 public class EditSubclassItemController extends HttpServlet {
 
-	public void doPost(HttpServletRequest req, HttpServletResponse res) {
-		String subclassURI = req.getParameter("subclassURI");
-		String superclassURI = req.getParameter("superclassURI");
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		String newSubclassURI = req.getParameter("newSubclassURI");
+		String oldSubclassURI = req.getParameter("oldSubclassURI");
+		String vclassURI = req.getParameter("superclassURI");
+		
+		WebappDaoFactory wadf = ModelAccess.on(getServletContext()).getWebappDaoFactory();
+        VClassDao vcwDao = wadf.getVClassDao();
+		
+		vcwDao.removeSubclass(vclassURI, oldSubclassURI);
+		vcwDao.addSubclass(vclassURI, newSubclassURI);
+		
+		res.getWriter().print(newSubclassURI);
 	}
 }
