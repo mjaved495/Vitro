4a9cd664e6bdeb5fddf839a9a9e5409f9aa5b68d
Amit Mizrahi <am2269@cornell.edu>
2015-11-06 17:42:57 -0500

    add/delete sub/superclasses

--- webapp/src/edu/cornell/mannlib/vitro/webapp/controller/edit/ajax/AddSuperpropertyItemController.java
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/controller/edit/ajax/AddSuperpropertyItemController.java
@@ -1,7 +1,26 @@
 package edu.cornell.mannlib.vitro.webapp.controller.edit.ajax;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.cornell.mannlib.vitro.webapp.dao.ObjectPropertyDao;
+import edu.cornell.mannlib.vitro.webapp.dao.VClassDao;
+import edu.cornell.mannlib.vitro.webapp.dao.WebappDaoFactory;
+import edu.cornell.mannlib.vitro.webapp.modelaccess.ModelAccess;
 
 public class AddSuperpropertyItemController extends HttpServlet {
-
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		String superpropertyURI = req.getParameter("superpropertyURI");
+		String propertyURI = req.getParameter("propertyURI");
+		
+		WebappDaoFactory wadf = ModelAccess.on(getServletContext()).getWebappDaoFactory();
+        ObjectPropertyDao opDao = wadf.getObjectPropertyDao();
+		
+		opDao.addSuperproperty(propertyURI, superpropertyURI);
+		
+		res.getWriter().print(superpropertyURI);
+	}
 }
--- webapp/src/edu/cornell/mannlib/vitro/webapp/controller/edit/ajax/DeleteSubpropertyItemController.java
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/controller/edit/ajax/DeleteSubpropertyItemController.java
@@ -1,7 +1,25 @@
 package edu.cornell.mannlib.vitro.webapp.controller.edit.ajax;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.cornell.mannlib.vitro.webapp.dao.ObjectPropertyDao;
+import edu.cornell.mannlib.vitro.webapp.dao.WebappDaoFactory;
+import edu.cornell.mannlib.vitro.webapp.modelaccess.ModelAccess;
 
 public class DeleteSubpropertyItemController extends HttpServlet {
-
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		String subpropertyURI = req.getParameter("subpropertyURI");
+		String propertyURI = req.getParameter("propertyURI");
+		
+		WebappDaoFactory wadf = ModelAccess.on(getServletContext()).getWebappDaoFactory();
+        ObjectPropertyDao opDao = wadf.getObjectPropertyDao();
+		
+		opDao.removeSubproperty(propertyURI, subpropertyURI);
+		
+		res.getWriter().print(subpropertyURI);
+	}
 }
