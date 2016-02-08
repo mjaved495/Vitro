8daf2b3cc0249dffe4b7adf323447a0e3d9db091
Amit Mizrahi <am2269@cornell.edu>
2016-02-08 14:48:57 -0500

    finish backend for getting object property data

--- webapp/src/edu/cornell/mannlib/vitro/webapp/controller/edit/ajax/GetPropHierarchyUtils.java
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/controller/edit/ajax/GetPropHierarchyUtils.java
@@ -4,18 +4,24 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import com.hp.hpl.jena.ontology.ConversionException;
+
 import edu.cornell.mannlib.vitro.webapp.beans.ObjectProperty;
 import edu.cornell.mannlib.vitro.webapp.dao.ObjectPropertyDao;
 
 public class GetPropHierarchyUtils {
 	
-	public class PropGraphNode {
+	public static class PropGraphNode {
 		List<PropGraphNode> incomingNeighbors;
 		List<PropGraphNode> outgoingNeighbors;
 		ObjectProperty prop;
 		
 		public PropGraphNode(ObjectProperty prop, List<PropGraphNode> incomingNeighbors, List<PropGraphNode> outgoingNeighbors) {
 			this.prop = prop;
+		}
+		
+		public ObjectProperty getProp() {
+			return this.prop;
 		}
 		
 	}
@@ -33,17 +39,33 @@ public class GetPropHierarchyUtils {
         return props;
     }
 	
-	public class PropGraph {
+	public static class PropGraph {
 		private List<PropGraphNode> nodes;
 		private ObjectPropertyDao opDao;  
 		
 		public PropGraph(ObjectPropertyDao opDao) {
 			this.opDao = opDao;
 			this.nodes = new ArrayList<PropGraphNode>();
+			
+			generateNodes();
+		}
+		
+		private void generateNodes() {
+			for(ObjectProperty prop : opDao.getAllObjectProperties()) {
+				addOrGetPropNode(prop);
+			}
 		}
 		
 		public PropGraphNode createPropGraphNode(ObjectProperty prop) {
-			List<ObjectProperty> superproperties = getPropsForURIList(opDao.getSuperPropertyURIs(prop.getURI(), true), opDao);
+			List<ObjectProperty> superproperties;
+			
+			try {
+				superproperties = getPropsForURIList(opDao.getSuperPropertyURIs(prop.getURI(), true), opDao);
+			}
+			catch(Exception e) {
+				superproperties = new ArrayList<ObjectProperty>();
+			}
+			
 			List<PropGraphNode> superPropNodes = new ArrayList<PropGraphNode>();
 			
 			for(ObjectProperty superprop : superproperties) {
@@ -70,9 +92,59 @@ public class GetPropHierarchyUtils {
 			
 			return createPropGraphNode(prop);
 		}
+		
+		public void addOrGetPropNode(ObjectProperty prop) {
+			for(PropGraphNode node : nodes) {
+				if(node.prop.equals(prop)) {
+					return;
+				}
+			}
+			
+			nodes.add(createPropGraphNode(prop));
+		}
+		
+		public PropHierarchyNode treeRoot() {
+			for(PropGraphNode node : nodes) {
+				if(node.incomingNeighbors.size() == 0) {
+					return new PropHierarchyNode(node.getProp());
+				}
+			}
+			return null;
+		}
+		
+		public PropHierarchyNode generateHierarchy() {
+			PropHierarchyNode result = new PropHierarchyNode("Property");
+			while(treeRoot() != null) {
+				PropHierarchyNode node = treeRoot();
+				result.addChild(dfsTree(node));
+			}
+			return result;
+		}
+		
+		public PropHierarchyNode dfsTree(PropHierarchyNode node) {
+			if(opDao.getSubPropertyURIs(node.getProp().getURI()).size() == 0) {
+				return node;
+			}
+			else {
+				for(String uri : opDao.getSubPropertyURIs(node.getProp().getURI())) {
+					node.addChild(dfsTree(new PropHierarchyNode(opDao.getObjectPropertyByURI(uri))));
+				}
+				return node;
+			}
+		}
 	}
 	
-	public static PropHierarchyNode generateFullTree() {
-		return null;
+	public static PropHierarchyNode generateFullTree(ObjectPropertyDao opDao) {
+		PropGraph pg = new PropGraph(opDao);
+		return pg.generateHierarchy();
+	}
+	
+	public static List<PropHierarchyNode> generatePropList(ObjectPropertyDao opDao) {
+		List<ObjectProperty> ops = opDao.getAllObjectProperties();
+		List<PropHierarchyNode> nodes = new ArrayList<PropHierarchyNode>();
+		for(ObjectProperty prop : ops) {
+			nodes.add(new PropHierarchyNode(prop));
+		}
+		return nodes;
 	}
 }
---
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/controller/edit/ajax/objproperty/ObjectPropertyInfoController.java
@@ -0,0 +1,150 @@
+package edu.cornell.mannlib.vitro.webapp.controller.edit.ajax.objproperty;
+
+import static edu.cornell.mannlib.vitro.webapp.modelaccess.ModelAccess.ReasoningOption.ASSERTIONS_ONLY;
+
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import edu.cornell.mannlib.vitro.webapp.beans.Ontology;
+import edu.cornell.mannlib.vitro.webapp.beans.VClass;
+import edu.cornell.mannlib.vitro.webapp.beans.ObjectProperty;
+import edu.cornell.mannlib.vitro.webapp.controller.Controllers;
+import edu.cornell.mannlib.vitro.webapp.controller.VitroRequest;
+import edu.cornell.mannlib.vitro.webapp.dao.ObjectPropertyDao;
+import edu.cornell.mannlib.vitro.webapp.dao.VClassDao;
+import edu.cornell.mannlib.vitro.webapp.dao.WebappDaoFactory;
+import edu.cornell.mannlib.vitro.webapp.modelaccess.ModelAccess;
+
+public class ObjectPropertyInfoController extends HttpServlet {
+	public void doGet (HttpServletRequest req, HttpServletResponse response) throws IOException {
+		Hashtable<String, Object> responseObject = new Hashtable<String, Object>();
+		
+		VitroRequest request = new VitroRequest(req);
+        
+        // get the VClass
+        
+        WebappDaoFactory wadf = ModelAccess.on(getServletContext()).getWebappDaoFactory(ASSERTIONS_ONLY);
+        
+        ObjectPropertyDao opDao = wadf.getObjectPropertyDao();
+        ObjectProperty op = (ObjectProperty)opDao.getObjectPropertyByURI(URLDecoder.decode(request.getParameter("uri"), "UTF-8"));
+        
+        VClassDao vcDao = wadf.getVClassDao();
+        
+        ObjectPropertyDao opwDao = wadf.getObjectPropertyDao();
+        ObjectPropertyDao displayOpDao = wadf.getObjectPropertyDao();
+      
+        Ontology ont = null;
+        
+        if(op.getNamespace() != null) {
+        	ont = wadf.getOntologyDao().getOntologyByURI(op.getNamespace());
+        }
+        
+        if(ont == null) {
+        	responseObject.put("ontology", "(unspecified)");
+        }
+        else {
+        	responseObject.put("ontology", ont.getName());
+        }
+       
+        String hiddenFromDisplay = (op.getHiddenFromDisplayBelowRoleLevel() == null ? "(unspecified)"
+				: op.getHiddenFromDisplayBelowRoleLevel().getDisplayLabel());
+		String prohibitedFromUpdate = (op
+				.getProhibitedFromUpdateBelowRoleLevel() == null ? "(unspecified)"
+				: op.getProhibitedFromUpdateBelowRoleLevel().getUpdateLabel());
+		String hiddenFromPublish = (op.getHiddenFromPublishBelowRoleLevel() == null ? "(unspecified)"
+				: op.getHiddenFromPublishBelowRoleLevel().getDisplayLabel());
+		
+		responseObject.put("displayLevel", hiddenFromDisplay);
+		responseObject.put("updateLevel", prohibitedFromUpdate);
+		responseObject.put("publishLevel", hiddenFromPublish);
+		
+        /*String group = (op.getGroup() == null ? "(unspecified)" : vcl.getGroup().getPublicName());
+        
+        responseObject.put("group", group);*/
+        
+        List<ObjectProperty> superproperties = getPropsForURIList(
+                opDao.getSuperPropertyURIs(op.getURI(),false), displayOpDao);
+        
+        List<Hashtable<String, String>> superpropertiesInfo = new ArrayList<Hashtable<String, String>>();
+        for(ObjectProperty prop : superproperties) {
+        	Hashtable<String, String> info = new Hashtable<String, String>();
+        	info.put("uri", prop.getURI());
+        	info.put("name", prop.getLabel());
+        	superpropertiesInfo.add(info);
+        }
+        
+        responseObject.put("superproperties", superpropertiesInfo);
+
+        List<ObjectProperty> subproperties = getPropsForURIList(
+                opDao.getSubPropertyURIs(op.getURI()), displayOpDao);
+        
+        List<Hashtable<String, String>> subpropertiesInfo = new ArrayList<Hashtable<String, String>>();
+        for(ObjectProperty prop: subproperties) {
+        	Hashtable<String, String> info = new Hashtable<String, String>();
+        	info.put("uri", prop.getURI());
+        	info.put("name", prop.getLabel());
+        	subpropertiesInfo.add(info);
+        }
+        
+        responseObject.put("subclasses", subpropertiesInfo);
+        
+        List<ObjectProperty> eqprops = getPropsForURIList(
+                opDao.getEquivalentPropertyURIs(op.getURI()), displayOpDao);
+        
+        List<Hashtable<String, String>> eqpropsInfo = new ArrayList<Hashtable<String, String>>();
+        for(ObjectProperty prop : eqprops) {
+        	Hashtable<String, String> info = new Hashtable<String, String>();
+        	info.put("uri", prop.getURI());
+        	info.put("name", prop.getLabel());
+        	eqpropsInfo.add(info);
+        }
+        
+        responseObject.put("eqprops", eqpropsInfo);
+        
+        /*List<ObjectProperty> invProps = getPropsForURIList(
+                opDao.getInversePropertyURIs(op.getURI()), displayOpDao); // this method does not exist...
+
+        List<Hashtable<String, String>> inversesInfo = new ArrayList<Hashtable<String, String>>();
+        for(ObjectProperty inverse : invProps) {
+        	Hashtable<String, String> info = new Hashtable<String, String>();
+        	info.put("uri", inverse.getURI());
+        	info.put("name", inverse.getName());
+        	inversesInfo.add(info);
+        } */
+        
+        /* responseObject.put("disjoints", disjointsInfo); */
+        
+        VClass domain = vcDao.getVClassByURI(op.getDomainVClassURI());
+        VClass range = vcDao.getVClassByURI(op.getRangeVClassURI());
+
+        responseObject.put("label", op.getLabel());
+        
+        Gson gson = new Gson();
+        response.getWriter().println(gson.toJson(responseObject));
+
+	}
+	
+	private static List<ObjectProperty> getPropsForURIList(List<String> propURIs, ObjectPropertyDao opDao) {
+        List<ObjectProperty> props = new ArrayList<ObjectProperty>();
+        Iterator<String> urIt = propURIs.iterator();
+        while (urIt.hasNext()) {
+            String propURI = urIt.next();
+            ObjectProperty op = opDao.getObjectPropertyByURI(propURI);
+            if (op != null) {
+                props.add(op);
+            }
+        }
+        return props;
+    }
+}
