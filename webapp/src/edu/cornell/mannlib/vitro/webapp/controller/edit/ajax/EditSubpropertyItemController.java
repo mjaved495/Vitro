82ce98e64914bba85fe1cea0466bfde3165e7fc5
Amit Mizrahi <am2269@cornell.edu>
2016-02-10 16:45:29 -0500

    implement editing subproperty

--- webapp/src/edu/cornell/mannlib/vitro/webapp/controller/edit/ajax/EditSubpropertyItemController.java
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/controller/edit/ajax/EditSubpropertyItemController.java
@@ -1,7 +1,27 @@
 package edu.cornell.mannlib.vitro.webapp.controller.edit.ajax;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.cornell.mannlib.vitro.webapp.dao.ObjectPropertyDao;
+import edu.cornell.mannlib.vitro.webapp.dao.WebappDaoFactory;
+import edu.cornell.mannlib.vitro.webapp.modelaccess.ModelAccess;
 
 public class EditSubpropertyItemController extends HttpServlet {
-
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		String oldSubpropertyURI = req.getParameter("oldSubpropertyURI");
+		String newSubpropertyURI = req.getParameter("newSubpropertyURI");
+		String propertyURI = req.getParameter("propertyURI");
+		
+		WebappDaoFactory wadf = ModelAccess.on(getServletContext()).getWebappDaoFactory();
+        ObjectPropertyDao opDao = wadf.getObjectPropertyDao();
+		
+		opDao.removeSubproperty(propertyURI, oldSubpropertyURI);
+		opDao.addSubproperty(propertyURI, newSubpropertyURI);
+		
+		res.getWriter().print(newSubpropertyURI);
+	}
 }
