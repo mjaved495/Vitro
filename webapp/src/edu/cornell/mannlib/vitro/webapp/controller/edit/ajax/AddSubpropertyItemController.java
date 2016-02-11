98da7ce3c49b5d89d0bbd5873a82c3ff9bf9a812
Amit Mizrahi <am2269@cornell.edu>
2016-02-11 16:08:04 -0500

    minor fix (shouldn't have >1 select)

--- webapp/src/edu/cornell/mannlib/vitro/webapp/controller/edit/ajax/AddSubpropertyItemController.java
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/controller/edit/ajax/AddSubpropertyItemController.java
@@ -6,11 +6,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import edu.cornell.mannlib.vitro.webapp.controller.edit.ClassPageController;
 import edu.cornell.mannlib.vitro.webapp.dao.ObjectPropertyDao;
 import edu.cornell.mannlib.vitro.webapp.dao.WebappDaoFactory;
 import edu.cornell.mannlib.vitro.webapp.modelaccess.ModelAccess;
 
 public class AddSubpropertyItemController extends HttpServlet {
+	private static final Log log = LogFactory.getLog(ClassPageController.class.getName());
+	
 	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		String subpropertyURI = req.getParameter("subpropertyURI");
 		String propertyURI = req.getParameter("propertyURI");
@@ -19,6 +25,7 @@ public class AddSubpropertyItemController extends HttpServlet {
         ObjectPropertyDao opDao = wadf.getObjectPropertyDao();
 		
 		opDao.addSubproperty(propertyURI, subpropertyURI);
+		log.debug(opDao.getAllSubPropertyURIs(propertyURI));
 		
 		res.getWriter().print(subpropertyURI);
 	}
