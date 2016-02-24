3c751653994d11bdd3661e570a63de8ed328517a
Amit Mizrahi <am2269@cornell.edu>
2016-02-24 16:42:31 -0500

    make progress on editing

--- webapp/src/edu/cornell/mannlib/vitro/webapp/controller/edit/ajax/dataproperty/EditDomainItemController.java
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/controller/edit/ajax/dataproperty/EditDomainItemController.java
@@ -1,5 +1,33 @@
 package edu.cornell.mannlib.vitro.webapp.controller.edit.ajax.dataproperty;
 
-public class EditDomainItemController {
+import java.io.IOException;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.cornell.mannlib.vitro.webapp.beans.DataProperty;
+import edu.cornell.mannlib.vitro.webapp.beans.ObjectProperty;
+import edu.cornell.mannlib.vitro.webapp.dao.DataPropertyDao;
+import edu.cornell.mannlib.vitro.webapp.dao.ObjectPropertyDao;
+import edu.cornell.mannlib.vitro.webapp.dao.VClassDao;
+import edu.cornell.mannlib.vitro.webapp.dao.WebappDaoFactory;
+import edu.cornell.mannlib.vitro.webapp.modelaccess.ModelAccess;
+
+public class EditDomainItemController extends HttpServlet {
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		String oldDomainURI = req.getParameter("oldDomainURI");
+		String newDomainURI = req.getParameter("newDomainURI");
+		String propertyURI = req.getParameter("propertyURI");
+		
+		WebappDaoFactory wadf = ModelAccess.on(getServletContext()).getWebappDaoFactory();
+        DataPropertyDao dpDao = wadf.getDataPropertyDao();
+		VClassDao vcDao = wadf.getVClassDao();
+        
+		DataProperty dp = dpDao.getDataPropertyByURI(propertyURI);
+		dp.setDomainVClassURI(newDomainURI);
+		dpDao.updateDataProperty(dp);
+		
+		res.getWriter().print(newDomainURI);
+	}
 }
--- webapp/src/edu/cornell/mannlib/vitro/webapp/controller/edit/ajax/dataproperty/EditSubpropertyItemController.java
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/controller/edit/ajax/dataproperty/EditSubpropertyItemController.java
@@ -1,5 +1,27 @@
 package edu.cornell.mannlib.vitro.webapp.controller.edit.ajax.dataproperty;
 
-public class EditSubpropertyItemController {
+import java.io.IOException;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.cornell.mannlib.vitro.webapp.dao.DataPropertyDao;
+import edu.cornell.mannlib.vitro.webapp.dao.WebappDaoFactory;
+import edu.cornell.mannlib.vitro.webapp.modelaccess.ModelAccess;
+
+public class EditSubpropertyItemController extends HttpServlet {
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		String oldSubpropertyURI = req.getParameter("oldSubpropertyURI");
+		String newSubpropertyURI = req.getParameter("newSubpropertyURI");
+		String propertyURI = req.getParameter("propertyURI");
+		
+		WebappDaoFactory wadf = ModelAccess.on(getServletContext()).getWebappDaoFactory();
+        DataPropertyDao dpDao = wadf.getDataPropertyDao();
+		
+		dpDao.removeSubproperty(propertyURI, oldSubpropertyURI);
+		dpDao.addSubproperty(propertyURI, newSubpropertyURI);
+		
+		res.getWriter().print(newSubpropertyURI);
+	}
 }
