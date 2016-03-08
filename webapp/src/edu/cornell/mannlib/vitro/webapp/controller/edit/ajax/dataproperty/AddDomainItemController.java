8a4fbae8a0331a7b6bd32692e6b2ccb3e594675e
Amit Mizrahi <am2269@cornell.edu>
2016-03-08 14:11:40 -0500

    continue working on data property

--- webapp/src/edu/cornell/mannlib/vitro/webapp/controller/edit/ajax/dataproperty/AddDomainItemController.java
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/controller/edit/ajax/dataproperty/AddDomainItemController.java
@@ -1,5 +1,42 @@
 package edu.cornell.mannlib.vitro.webapp.controller.edit.ajax.dataproperty;
 
-public class AddDomainItemController {
+import java.io.IOException;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import edu.cornell.mannlib.vitro.webapp.beans.DataProperty;
+import edu.cornell.mannlib.vitro.webapp.beans.ObjectProperty;
+import edu.cornell.mannlib.vitro.webapp.controller.edit.ClassPageController;
+import edu.cornell.mannlib.vitro.webapp.dao.DataPropertyDao;
+import edu.cornell.mannlib.vitro.webapp.dao.ObjectPropertyDao;
+import edu.cornell.mannlib.vitro.webapp.dao.VClassDao;
+import edu.cornell.mannlib.vitro.webapp.dao.WebappDaoFactory;
+import edu.cornell.mannlib.vitro.webapp.modelaccess.ModelAccess;
+
+public class AddDomainItemController extends HttpServlet {
+	
+	private static final Log log = LogFactory.getLog(AddDomainItemController.class.getName());
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		String domainURI = req.getParameter("domainURI");
+		String propertyURI = req.getParameter("propertyURI");
+		
+		WebappDaoFactory wadf = ModelAccess.on(getServletContext()).getWebappDaoFactory();
+		DataPropertyDao dpDao = wadf.getDataPropertyDao();
+		
+		log.info(domainURI);
+		
+		DataProperty dp = dpDao.getDataPropertyByURI(propertyURI);
+		dp.setDomainVClassURI(domainURI);
+		//dpDao.updateDataProperty(dp);
+		
+		log.info(dp.getDomainVClassURI());
+		
+		res.getWriter().print(domainURI);
+	}
 }
--- webapp/src/edu/cornell/mannlib/vitro/webapp/controller/edit/ajax/dataproperty/DeleteEqPropertyItemController.java
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/controller/edit/ajax/dataproperty/DeleteEqPropertyItemController.java
@@ -1,5 +1,25 @@
 package edu.cornell.mannlib.vitro.webapp.controller.edit.ajax.dataproperty;
 
-public class DeleteEqPropertyItemController {
+import java.io.IOException;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.cornell.mannlib.vitro.webapp.dao.DataPropertyDao;
+import edu.cornell.mannlib.vitro.webapp.dao.WebappDaoFactory;
+import edu.cornell.mannlib.vitro.webapp.modelaccess.ModelAccess;
+
+public class DeleteEqPropertyItemController extends HttpServlet {
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		String eqPropertyURI = req.getParameter("eqPropertyURI");
+		String propertyURI = req.getParameter("propertyURI");
+		
+		WebappDaoFactory wadf = ModelAccess.on(getServletContext()).getWebappDaoFactory();
+        DataPropertyDao dpDao = wadf.getDataPropertyDao();
+		
+		dpDao.removeEquivalentProperty(propertyURI, eqPropertyURI);
+		
+		res.getWriter().print(eqPropertyURI);
+	}
 }
