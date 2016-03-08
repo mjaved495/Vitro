39d094ee72d93e3c8c22f1f9ff5f9c2224849ad8
Amit Mizrahi <am2269@cornell.edu>
2016-03-08 13:18:18 -0500

    fix issues with data property page

--- webapp/src/edu/cornell/mannlib/vitro/webapp/controller/edit/ajax/dataproperty/DeleteSubpropertyItemController.java
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/controller/edit/ajax/dataproperty/DeleteSubpropertyItemController.java
@@ -1,5 +1,26 @@
 package edu.cornell.mannlib.vitro.webapp.controller.edit.ajax.dataproperty;
 
-public class DeleteSubpropertyItemController {
+import java.io.IOException;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.cornell.mannlib.vitro.webapp.dao.DataPropertyDao;
+import edu.cornell.mannlib.vitro.webapp.dao.ObjectPropertyDao;
+import edu.cornell.mannlib.vitro.webapp.dao.WebappDaoFactory;
+import edu.cornell.mannlib.vitro.webapp.modelaccess.ModelAccess;
+
+public class DeleteSubpropertyItemController extends HttpServlet {
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		String subpropertyURI = req.getParameter("subpropertyURI");
+		String propertyURI = req.getParameter("propertyURI");
+		
+		WebappDaoFactory wadf = ModelAccess.on(getServletContext()).getWebappDaoFactory();
+        DataPropertyDao dpDao = wadf.getDataPropertyDao();
+		
+		dpDao.removeSubproperty(propertyURI, subpropertyURI);
+		
+		res.getWriter().print(subpropertyURI);
+	}
 }
